1af0f9d5e75bf833437ef745273b3c3d8b46f7d6
Patryk <patrykrudzinski@gmail.com>
2018-05-10 15:06:19 +0200

    levels

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
+    setTimeout(function(){
+        boom.style.transform = 'scale(0)';
+        document.querySelector('#loader').style.display = 'none';
+    }, 1000);
 
     const board = document.querySelector('#board');
     const boom = document.querySelector('#boom');
     const pause = document.querySelector('#pause');
+    const start = document.querySelector('#start');
+    const end = document.querySelector('#end');
     const hp = document.querySelector('#hp');
     const score = document.querySelector('#score>span');
 
@@ -30,71 +37,85 @@ document.addEventListener('DOMContentLoaded', function () {
 
     generateHp();
 
-    document.addEventListener('mouseenter', function(){
-        [...document.querySelectorAll('.zombie')].forEach(el=>el.style.animationPlayState = 'running');
-        pause.style.display = 'none';
+    document.addEventListener('mouseenter', function() {
 
-        const time = setInterval(function () {
+            [...document.querySelectorAll('.zombie')].forEach(el => el.style.animationPlayState = 'running');
+            pause.style.display = 'none';
 
-            const zombie = document.createElement('div');
-            zombie.classList.add('zombie');
+            const time = setInterval(function () {
 
-            const bottomPos = Math.floor(Math.random()*(150));
-            zombie.style.bottom = bottomPos + 'px';
-            zombie.style.zIndex = 150 - bottomPos + '';
+                const zombie = document.createElement('div');
+                zombie.classList.add('zombie');
 
-            let min = 0.2;
-            let max = 1.1;
-            const scale = Math.random()*(max - min) + min;
-            zombie.style.transform = 'scale(' + scale + ')';
+                const bottomPos = Math.floor(Math.random() * (150));
+                zombie.style.bottom = bottomPos + 'px';
+                zombie.style.zIndex = 150 - bottomPos + '';
+
+                let min = 0.2;
+                let max = 1.1;
+                const scale = Math.random() * (max - min) + min;
+                zombie.style.transform = 'scale(' + scale + ')';
 
 
-            const walkSpeed = scale * 30 + 5;
-            const anim = '0.5s, '+ walkSpeed + 's';
-            zombie.style.animationDuration = anim;
+                let walkSpeed = 5;
+                const speed = scale * 30 - (level * 2);
+                if (speed > 0) walkSpeed += speed;
+                console.log(walkSpeed);
+                const anim = '0.5s, ' + walkSpeed + 's';
+                zombie.style.animationDuration = anim;
 
-            board.appendChild(zombie);
+                board.appendChild(zombie);
 
-            zombie.addEventListener('animationend', function (e) {
-                if(e.animationName === 'zombieWalk') {
-                    board.removeChild(this);
-                    board.classList.add('shake');
+                zombie.addEventListener('animationend', function (e) {
+                    if (e.animationName === 'zombieWalk') {
+                        board.removeChild(this);
+                        board.classList.add('shake');
+                        setTimeout(function () {
+                            board.classList.remove('shake');
+                        }, 500);
+                        life--;
+                        generateHp();
+                        if (life <= 0) {
+                            window.clearInterval(time);
+                            end.style.display = 'flex';
+                            end.children[1].children[0].innerText = points;
+                            [...document.querySelectorAll('.zombie')].forEach(el => el.style.animationPlayState = 'paused')
+                        }
+                    }
+                });
+
+                zombie.addEventListener('click', function (e) {
+                    boom.style.transform = 'translateY(' + (e.screenY - 140 - board.offsetTop) + 'px)';
+                    boom.style.transform += 'translateX(' + (e.screenX - 40 - board.offsetLeft) + 'px)';
+
+                    const zombiePosition = window.getComputedStyle(this).left;
+                    this.style.left = zombiePosition + 'px';
+                    this.style.animationPlayState = 'paused';
+                    this.style.transform += 'rotate(90deg)';
+
                     setTimeout(function () {
-                        board.classList.remove('shake');
-                    }, 500);
-                    life--;
-                    generateHp();
-                }
+                        e.target.remove()
+                    }, 600);
+
+                    setTimeout(function () {
+                        boom.style.transform = 'scale(0)';
+                    }, 170);
+
+                    points++;
+                    if (points % 3 == 0) level++;
+                    console.log(level);
+                    updateScore();
+
+                })
+
+            }, 1000);
+
+
+            document.addEventListener('mouseleave', function () {
+                window.clearInterval(time);
+                pause.style.display = 'flex';
+                [...document.querySelectorAll('.zombie')].forEach(el => el.style.animationPlayState = 'paused')
             });
 
-            zombie.addEventListener('click', function (e) {
-                boom.style.transform = 'translateY('+(e.screenY-140-board.offsetTop)+'px)';
-                boom.style.transform += 'translateX('+(e.screenX-40-board.offsetLeft)+'px)';
-
-                const zombiePosition = window.getComputedStyle(this).left;
-                this.style.left = zombiePosition + 'px';
-                this.style.animationPlayState = 'paused';
-                this.style.transform += 'rotate(90deg)';
-
-                setTimeout(function(){
-                    e.target.remove()
-                }, 600);
-
-                setTimeout(function () {
-                    boom.style.transform = 'scale(0)';
-                }, 170);
-
-                points++;
-                updateScore();
-
-            })
-
-        }, 1000);
-
-        document.addEventListener('mouseleave', function(){
-            window.clearInterval(time);
-            pause.style.display = 'flex';
-            [...document.querySelectorAll('.zombie')].forEach(el=>el.style.animationPlayState = 'paused')
-        });
     });
-});
+})
--- readme.md
+++ readme.md
@@ -5,7 +5,5 @@
 [Live](https://patrykrudzinski.github.io/Summer_zombies/)
 
  ToDo:
- * loader (pokazac boom)
- * increasing levels
  * start/end game
- * svg palms    
+ * rekfractoring
